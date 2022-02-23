69ae3c31eeb48880e2ad5a35345308aa22914a32
nathalia <nathaliatroina@gmail.com>
2022-02-23 11:02:37 -0500

    commit tree 5b8b85c8298c100b4233aac42dfb7df9cb996f82

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+# setter mathod
+def name=(name)
+    @name = name
+end 
+#getter method
+def name 
+    @name
 end
+
+def breed=(breed)
+    @breed = breed
+end
+
+def breed 
+    @breed
+end    
+    end
+
